cc557696805fab676c9eb027271feaf1104d8d2c
Paulo Vieira <paullocostta86@gmail.com>
2021-03-03 01:29:23 -0300

    :sparkles: nextjs boilerplate 1.0.0

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,18 @@
+import * as S from './styles'
+
+const Main = ({
+  title = 'Boilerplate Next.js',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Imagem de um átomo." />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
+  </S.Wrapper>
+)
+
+export default Main
