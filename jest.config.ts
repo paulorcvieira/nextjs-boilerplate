9f7f7e8837fd5faa059a4a4bdcc82fe34360f869
Paulo Vieira <paullocostta86@gmail.com>
2021-05-12 10:25:00 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/'],
+};
