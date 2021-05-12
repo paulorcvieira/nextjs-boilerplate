5bfa920637f12dc3e4c8fd35092bad8093cf0cf6
Paulo Vieira <paullocostta86@gmail.com>
2021-05-12 07:05:15 -0300

    chore: create project with next-app

---
+++ scr/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Boilerplate</title>
+        <meta name="description" content="Boilerplate" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>
+        Welcome to Boilerplate
+      </h1>
+    </div>
+  )
+}
